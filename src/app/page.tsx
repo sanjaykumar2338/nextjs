e0a7c5e02e09c77b361274562aa133cbade213f6
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2025-09-23 13:54:40 +0530

    component removed from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Location from "@/components/homes/homepage-1/Location";
 //import Process from "@/components/homes/homepage-1/Process";
 import Properties from "@/components/homes/homepage-1/Properties";
 import Properties2 from "@/components/homes/homepage-1/Properties2";
-import Testimonials from "@/components/homes/homepage-1/Testimonials";
+//import Testimonials from "@/components/homes/homepage-1/Testimonials";
 import LatestNews from "@/components/homes/LatestNews";
 import Layout from "@/components/layouts/Layout-defaul";
 
@@ -18,7 +18,6 @@ export default function Home() {
             <Properties />
             <Properties2 />
             <Location />
-            <Testimonials />
             <LatestNews />
         </Layout>
     );
